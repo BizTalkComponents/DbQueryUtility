14d4b41f98a7bc133e0332505acf3eb3953ee517
Michael Ha <michael.ha@ibiz-solutions.no>
2017-08-08 10:18:35 +0200

    Intial commit for supporting Oracle queries

---
+++ Src/DbQueryUtility/OracleDbQueryApplicationService.cs
@@ -0,0 +1,32 @@
+﻿using BizTalkComponents.Utilities.DbQueryUtility.Repository;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace BizTalkComponents.Utilities.DbQueryUtility
+{
+    class OracleDbQueryApplicationService
+    {
+        public XPathNavigator Query(string query, string configurationKey)
+        {
+            var util = new DbQueryUtilityService(new OracleDbQueryRepository());
+
+            XPathNavigator nav;
+
+            try
+            {
+                nav = util.Query(query, configurationKey);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(String.Format("An exception was thrown in DbQueryUtity. {0}", ex.ToString()));
+                throw ex;
+            }
+            return nav;
+        }
+    }
+}
---
+++ Src/DbQueryUtility/Repository/OracleDbQueryRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client; 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace BizTalkComponents.Utilities.DbQueryUtility.Repository
+{
+    public class OracleDbQueryRepository : IDbQueryRepository
+    {
+        public XDocument Query(string query, string configurationKey)
+        {
+            var doc = new XDocument();
+            var resultElement = new XElement("Result");
+            var connectionString = ConfigurationManager.ConnectionStrings[configurationKey].ConnectionString;
+
+            using (var connection = new OracleConnection(connectionString))
+            {
+                using (OracleCommand command = new OracleCommand (query, connection))
+                {
+                    if (!query.Contains(' '))
+                    {
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+                    }
+                    else
+                    {
+                        command.CommandType = System.Data.CommandType.Text;
+                    }
+
+                    OracleDataReader reader;
+
+                    connection.Open();
+
+                    using (reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                resultElement.Add(new XElement(reader.GetName(i), reader.GetValue(i)));
+                            }
+                        }
+                    }
+                }
+            }
+
+            doc.Add(resultElement);
+
+            return doc;
+        }
+
+    }
+}
